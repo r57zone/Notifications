e34a42c3244fcd28ad2a2d5c7a4c213d66aefcf1
r57zone <r57zone@gmail.com>
2016-04-28 22:45:49 +0400

    update

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 <h2>RU:</h2>
-Уведомления в стиле Windows 10, Windows 8.1, Windows 8 (Toast Notification) для своих приложений.<br>
+Уведомления в стиле Windows 10, Windows 8.1, Windows 8 (Toast Notification) для своих приложений. Совместимо с [альтернативным центром уведомлений](https://github.com/r57zone/Notification-center).<br>
 ![1](https://cloud.githubusercontent.com/assets/9499881/8044777/01414cc8-0e40-11e5-8288-0b42f5a49e0a.png)<br>
 **Параметры запуска:**<br>
 1) Заголовок уведомления (полужирный шрифт). Если не нужен, то написать "null";<br>
 2) Первая строка. Если не нужна, то написать "null";<br>
 3) Вторая строка. Если не нужна, то написать "null";<br>
-4) Название файлы иконки слева (90 на 90 пикселей), закинуть в папку с приложением файл формата png, jpg, bmp или gif. Если не нужна, то написать "null";<br>
-5) Название файлы иконки справа (30 на 30 пикселей), закинуть в папку с приложением файл формата png, jpg, bmp или gif. Если не нужна, то написать "null";<br>
+4) Название файла иконки слева (90 на 90 пикселей), закинуть в папку с приложением файл формата png, jpg, bmp или gif. Если не нужна, то написать "null";<br>
+5) Название файла иконки справа (30 на 30 пикселей), закинуть в папку с приложением файл формата png, jpg, bmp или gif. Если не нужна, то написать "null";<br>
 6) Цвет темы в цифрах, от 0 до 6; Задавать не обязательно, по умолчанию будет тема 0. Светло-синий - 0, Темно-синий - 1, Сине-зеленый - 2, Зеленый - 3, Фиолетовый - 4, Темно-розовый - 5, Черный - 6; 
 
 **Пример:**<br>
@@ -26,14 +26,14 @@
 **Обратная связь**: r57zone[собака]gmail.com 
 
 <h2>EN:</h2>
-Notifications in the style of Windows 10, Windows 8.1, Windows 8 (Toast Notification) for their applications.<br>
+Notifications in the style of Windows 10, Windows 8.1, Windows 8 (Toast Notification) for their applications. Compatible with [alternative center of notifications](https://github.com/r57zone/Notification-center).<br>
 
 **Startup parameters**<br>
 1) Headline Notification (bold). If you do not need, then write "null";<br>
 2) The first line. If you do not need, then write "null";<br>
 3) The second line. If you do not need, then write "null";<br>
 4) The file name icon on the left (90 by 90 pixels), throw in a folder with the application file format png, jpg, bmp or gif. If you do not need, then write "null";<br>
-5) Name of file icons on the right (30 30 pixels), throw in a folder with the application file format png, jpg, bmp or gif. If you do not need, then write "null";<br>
+5) The file name icon on the right (30 30 pixels), throw in a folder with the application file format png, jpg, bmp or gif. If you do not need, then write "null";<br>
 6) The color of threads in the figures from 0 to 6; Asking is not necessarily the default theme 0. Light blue - 0 Dark Blue - 1, Blue-Green - 2 Green - 3, Purple - 4, Dark Pink - 5, Black - 6; 
 
 **Example:**<br>
--- Unit1.pas
+++ Unit1.pas
@@ -2,6 +2,9 @@ unit Unit1;
 
 interface
 
+{Notifications 0.3, последнее обновление 28.04.2016
+https://github.com/r57zone/notifications}
+
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, ExtCtrls, StdCtrls, MMSystem, jpeg, pngimage;
@@ -45,125 +48,147 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 var
-WND:HWND; Path:string;
+  WND:HWND; Path, Command:string;
+  CDS:TCopyDataStruct;
 begin
-WND:=FindWindow(nil, 'Notification Show');
-if WND<>0 then while WND<>0 do begin sleep(100); WND:=FindWindow(nil, 'Notification Show'); end;
-Caption:='Notification Show';
-Form1.Left:=Screen.Width-Form1.Width;
-Form1.Top:=Screen.Height-Form1.Height-57;
-ThemeColor:=0;
+  WND:=FindWindow(nil, 'Notification Show');
+  if WND<>0 then while WND<>0 do begin sleep(100); WND:=FindWindow(nil, 'Notification Show'); end;
+  Caption:='Notification Show';
+  Form1.Left:=Screen.Width-Form1.Width;
+  Form1.Top:=Screen.Height-Form1.Height-57;
+  ThemeColor:=0;
 
-if (ParamStr(1)<>'') and (ParamStr(1)<>'null') then
-Label1.Caption:=ParamStr(1);
-if (ParamStr(2)<>'') and (ParamStr(2)<>'null') then
-Label2.Caption:=ParamStr(2);
-if (ParamStr(3)<>'') and (ParamStr(3)<>'null') then
-Label3.Caption:=ParamStr(3);
+  if (ParamStr(1)<>'') and (ParamStr(1)<>'null') then
+    Label1.Caption:=ParamStr(1);
+  if (ParamStr(2)<>'') and (ParamStr(2)<>'null') then
+    Label2.Caption:=ParamStr(2);
+  if (ParamStr(3)<>'') and (ParamStr(3)<>'null') then
+    Label3.Caption:=ParamStr(3);
 
-if (ParamStr(4)<>'') and (ParamStr(4)<>'null') then
-if FileExists(ExtractFilePath(ParamStr(0))+'\'+ParamStr(4)) then
-if (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.jpg') or (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.png')
-or (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.bmp') or (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.gif') then begin
-Image2.Picture.LoadFromFile(ExtractFilePath(ParamStr(0))+'\'+ParamStr(4));
-Label1.Left:=100;
-Label2.Left:=100;
-Label3.Left:=100;
-end;
+  if (ParamStr(4)<>'') and (ParamStr(4)<>'null') then
+  if FileExists(ExtractFilePath(ParamStr(0))+'\'+ParamStr(4)) then
+  if (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.jpg') or (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.png')
+  or (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.bmp') or (AnsiLowerCase(ExtractFileExt(ParamStr(4)))='.gif') then begin
+    Image2.Picture.LoadFromFile(ExtractFilePath(ParamStr(0))+ParamStr(4));
+    Label1.Left:=100;
+    Label2.Left:=100;
+    Label3.Left:=100;
+  end;
 
-if (ParamStr(5)<>'') and (ParamStr(5)<>'null') then
-if FileExists(ExtractFilePath(ParamStr(0))+'\'+ParamStr(5)) then
-if (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.jpg') or (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.png')
-or (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.bmp') or (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.gif') then
-Image1.Picture.LoadFromFile(ExtractFilePath(ParamStr(0))+'\'+ParamStr(5));
+  if (ParamStr(5)<>'') and (ParamStr(5)<>'null') then
+  if FileExists(ExtractFilePath(ParamStr(0))+'\'+ParamStr(5)) then
+  if (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.jpg') or (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.png')
+  or (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.bmp') or (AnsiLowerCase(ExtractFileExt(ParamStr(5)))='.gif') then
+    Image1.Picture.LoadFromFile(ExtractFilePath(ParamStr(0))+ParamStr(5));
 
-if ParamStr(6)<>'' then ThemeColor:=StrToInt(ParamStr(6));
-SetWindowLong(Application.Handle, GWL_EXSTYLE,GetWindowLong(Application.Handle, GWL_EXSTYLE) or WS_EX_TOOLWINDOW);
+  if ParamStr(6)<>'' then ThemeColor:=StrToInt(ParamStr(6));
+  SetWindowLong(Application.Handle, GWL_EXSTYLE,GetWindowLong(Application.Handle, GWL_EXSTYLE) or WS_EX_TOOLWINDOW);
 
-case ThemeColor of
-0: Form1.Color:=RGB(0,172,238); //Светло-синий
-1: Form1.Color:=RGB(35,93,130); //Темно-синий
-2: Form1.Color:=RGB(1,131,153); //Сине-зеленый
-3: Form1.Color:=RGB(0,138,0);   //Зеленый
-4: Form1.Color:=RGB(81,51,171); //Фиолетовый
-5: Form1.Color:=RGB(139,0,148); //Темно-розовый
-6: Form1.Color:=RGB(34,34,34); //Черный
-end;
+  case ThemeColor of
+    0: Form1.Color:=RGB(0,172,238); //Светло-синий
+    1: Form1.Color:=RGB(35,93,130); //Темно-синий
+    2: Form1.Color:=RGB(1,131,153); //Сине-зеленый
+    3: Form1.Color:=RGB(0,138,0);   //Зеленый
+    4: Form1.Color:=RGB(81,51,171); //Фиолетовый
+    5: Form1.Color:=RGB(139,0,148); //Темно-розовый
+    6: Form1.Color:=RGB(34,34,34); //Черный
+  end;
+
+  WND:=FindWindow('TForm1', 'Notification center');
+  if WND<>0 then begin
+    CDS.dwData:=0;
+    Command:='NOTIFY ';
+    if (ParamStr(1)<>'') and (ParamStr(1)<>'null') then
+      Command:=Command+'"'+ParamStr(1)+'" ' else Command:=Command+'"null" ';
+    if (ParamStr(2)<>'') and (ParamStr(2)<>'null') then
+      Command:=Command+'"'+ParamStr(2)+'" ' else Command:=Command+'"null" ';
+    if (ParamStr(3)<>'') and (ParamStr(3)<>'null') then
+      Command:=Command+'"'+ParamStr(3)+'" ' else Command:=Command+'"null" ';
+    if (ParamStr(4)<>'') and (ParamStr(4)<>'null') then
+      Command:=Command+'"'+ExtractFilePath(ParamStr(0))+ParamStr(4)+'" ' else Command:=Command+'"null" ';
+    if (ParamStr(5)<>'') and (ParamStr(5)<>'null') then
+      Command:=Command+'"'+ExtractFilePath(ParamStr(0))+ParamStr(5)+'" ' else Command:=Command+'"null" ';
+    if (ParamStr(6)<>'') and (ParamStr(6)<>'null') then
+      Command:=Command+'"'+ParamStr(6)+'" ' else Command:=Command+'"null"';
+    CDS.cbData:=(length(Command)+ 1)*sizeof(char);
+    CDS.lpData:=PChar(Command);
+    SendMessage(WND,WM_COPYDATA, Integer(Handle), Integer(@CDS));
+  end;
 end;
 
 procedure TForm1.FormPaint(Sender: TObject);
 begin
-case ThemeColor of
-0: Canvas.Pen.Color:=RGB(48,186,238);
-1: Canvas.Pen.Color:=RGB(76,122,152);
-2: Canvas.Pen.Color:=RGB(37,148,168);
-3: Canvas.Pen.Color:=RGB(48,158,48);
-4: Canvas.Pen.Color:=RGB(127,108,186);
-5: Canvas.Pen.Color:=RGB(159,47,166);
-6: Canvas.Pen.Color:=RGB(75,75,75);
-end;
-Canvas.Pen.Width:=2;
-Canvas.MoveTo(Width,1);
-Canvas.LineTo(0,1);
-Canvas.LineTo(1,Height-1);
-Canvas.LineTo(Width,Height-1);
+  case ThemeColor of
+    0: Canvas.Pen.Color:=RGB(48,186,238);
+    1: Canvas.Pen.Color:=RGB(76,122,152);
+    2: Canvas.Pen.Color:=RGB(37,148,168);
+    3: Canvas.Pen.Color:=RGB(48,158,48);
+    4: Canvas.Pen.Color:=RGB(127,108,186);
+    5: Canvas.Pen.Color:=RGB(159,47,166);
+    6: Canvas.Pen.Color:=RGB(75,75,75);
+  end;
+  Canvas.Pen.Width:=2;
+  Canvas.MoveTo(Width,1);
+  Canvas.LineTo(0,1);
+  Canvas.LineTo(1,Height-1);
+  Canvas.LineTo(Width,Height-1);
 end;
 
 function GetWindowsDir:string;
 var
-name:array [0..255] of Char;
+  name:array [0..255] of Char;
 begin
-GetWindowsDirectory(Name, SizeOf(Name));
-Result:=name;
+  GetWindowsDirectory(Name, SizeOf(Name));
+  Result:=name;
 end;
 
 procedure TForm1.FormShow(Sender: TObject);
 begin
 //AW_SLIDE, AW_ACTIVATE, AW_BLEND, AW_HIDE, AW_CENTER, AW_HOR_POSITIVE, AW_HOR_NEGATIVE, AW_VER_POSITIVE, AW_VER_NEGATIVE
-SetForegroundWindow(Application.Handle);
-AnimateWindow(Handle, 500, AW_BLEND);
-PlaySound(PChar(GetWindowsDir+'\Media\notify.wav'), 0, SND_ASYNC);
-Timer1.Enabled:=True;
+  SetForegroundWindow(Application.Handle);
+  AnimateWindow(Handle, 500, AW_BLEND);
+  PlaySound(PChar(GetWindowsDir+'\Media\notify.wav'), 0, SND_ASYNC);
+  Timer1.Enabled:=True;
 end;
 
 procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
-AnimateWindow(handle, 500, AW_BLEND or AW_HIDE);
+  AnimateWindow(handle, 500, AW_BLEND or AW_HIDE);
 end;
 
 procedure TForm1.Label1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-Close;
+  Close;
 end;
 
 procedure TForm1.Label2MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-Close;
+  Close;
 end;
 
 procedure TForm1.Label3MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-Close;
+  Close;
 end;
 
 procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-Close;
+  Close;
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 begin
-Close;
+  Close;
 end;
 
 procedure TForm1.Image2MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-Close;
+  Close;
 end;
 
 end.
